cb012f27f698d43619a7e4b45c058e327742910f
Kevin Denamganai <denamganai.kevin@gmail.com>
2019-03-07 14:12:41 +0000

    changed the demo file to take into account the Tuple action_space + run for 1 episode alone.

--- agent_zoo/demo_robosumo.py
+++ agent_zoo/demo_robosumo.py
@@ -7,9 +7,11 @@ env.reset()
 while True:
 	# The first sumo does not move, while the second moves randomly:
 	action = env.action_space.sample()
-	action[0:8] = 0.0
+	#action[0][0:8] = 0.0
 	state, reward, done, info = env.step(action)
 	env.render()
 
 	if done :
 		env.reset()
+		break
+		#print('environment resetted...')
