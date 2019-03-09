fb8d9c155690fbffccae57622a5c583be31baee3
KY Denamganai <kyd500@login1.pri.viking.alces.network>
2019-03-09 13:58:52 +0000

    configuration that saw a success on viking.

--- agent_zoo/demo_robosumo.py
+++ agent_zoo/demo_robosumo.py
@@ -1,4 +1,4 @@
-from OpenGL import GL 
+#from OpenGL import GL 
 import roboschool
 import gym
 import numpy as np 
@@ -19,12 +19,12 @@ while True:
                         [state[0][2],state[1][2] ]])
     q = np.array([ [state[0][3],state[1][3] ],
                         [state[0][4],state[1][4] ],
-                        [state[0][5],state[1][5] ],
+                        [state[-1][5],state[1][5] ],
                         [state[0][6],state[1][6] ]])
-    #print(q)
-    #print('--'*20)
-    env.render()
-    #time.sleep(1)
+    print(q)
+    print('--'*20)
+    #env.render()
+    time.sleep(1)
     
     if done :
         env.reset()
---
+++ viking_config/commands_robosumo.sh
@@ -0,0 +1,3 @@
+export CPLUS_INCLUDE_PATH=~/scartch/roboschool/venv/include/python3.6m/
+source ./exports.sh
+pip install boost
